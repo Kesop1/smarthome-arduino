ea1ac7e853990eefd645ffd3e24808b9683e2e29
Kesop1 <kesop1@gmail.com>
2023-02-26 09:31:34 +0100

    rewrite
    added NTC thermistor algorithm for floor sensor, algorithm works, but still needs some polishing
    TODOs added

--- rancho/domek-mega.ino
+++ rancho/domek-mega.ino
@@ -1,234 +1,110 @@
+#include <ArduinoSTL.h>  // Workaround: downgrade Arduino AVR Boards to 1.8.2. https://github.com/mike-matera/ArduinoSTL/issues/56
+#include <string.h>
+#include <map>
 #include <Ethernet.h>
 #include <PubSubClient.h>
 #include <DHT.h>
 
-class ShutterRelay {
-  public:
-    ShutterRelay(uint8_t lowPin, uint8_t highPin, long operationDuration, PubSubClient *client, String topic) : lowPin_(lowPin), highPin_(highPin), operationDuration_(operationDuration), 
-    client_(client), topic_(topic) { 
-      pinMode(lowPin_, OUTPUT);
-      pinMode(highPin_, OUTPUT);
-    }
+#define RELAY_DEFAULT_OFF HIGH
+#define M10_HIGH 21
+#define M10_LOW 22
 
-    /**
-      control shutter relay based on message, first deactivate(reset) pins to avoid damaging the relay 
-    */
-    void control(String message) {
-      Serial.println("Shutter control requested: go " + message);
-      resetState();
-      if(message == "UP") {
-        activate();
-        digitalWrite(highPin_, LOW);
-        setStatus("GOING_UP");
-        lastOperationUp_ = true;
-      } else if(message == "DOWN") {
-        activate();
-        digitalWrite(lowPin_, LOW);
-        setStatus("GOING_DOWN");
-        lastOperationUp_ = false;
-      } else {
-        setStatus("MIDDLE");
-        deactivate();
-      }
-    }
-
-    /**
-      reset the pins to avoid damaging the relay 
-    */
-    void resetState() {
-      digitalWrite(highPin_, HIGH);
-      digitalWrite(lowPin_, HIGH);
-    }
-
-    /**
-      subscribe to MQTT topic
-    */
-    void connect() {
-      char topic[20];
-      snprintf(topic, 20, "/%d", topic_);
-      client_->subscribe(topic);
-    }
-
-    /**
-      publish shutter status to MQTT topic
-    */
-    void setStatus(String status) {
-      char topic[20];
-      snprintf(topic, 20, "/%d/status", topic_);
-      char payload[20];
-      snprintf(payload, 20, "%s", status.c_str());
-      client_->publish(topic, payload);
-    }
-
-    /**
-      mark the shutter as active, performing operaion
-    */
-    void activate() {
-      active_ = true;
-      lastOperationStartTime_ = millis();
-    }
-
-    /**
-      mark the shutter as inactive and reset the pins
-    */
-    void deactivate() {
-      active_ = false;
-      resetState();
-    }
-
-    bool isActive() {
-      return active_;
-    }
-
-    /**
-      check if the shutter is still performing its last operation (operation duration may vary based on the shutter's size). 
-      if the max operation duration is exceeded the shutter has finished its operationfor sure, so deactivate it
-      and set the status as finished based on the last opeartion
-    */
-    void checkActive() {
-      if(active_ && (millis() - lastOperationStartTime_ > operationDuration_)) {
-        deactivate();
-        setStatus(lastOperationUp_ ? "UP" : "DOWN");
-      }
-    }
-
-  private:
-    uint8_t lowPin_;
-    uint8_t highPin_;
-    long operationDuration_;
-    PubSubClient *client_;
-    String topic_;
-    bool active_;
-    long lastOperationStartTime_;
-    bool lastOperationUp_;
-};
+std::map<std::string, int> shutterRelaysHighMap;
+std::map<std::string, int> shutterRelaysLowMap;
 
 unsigned long currentMillis;
 
 // Replace with your network details
-byte mac[] = { 0xDE, 0xAD, 0xBE, 0xEF, 0xFE, 0xED };
-IPAddress ip(192, 168, 0, 103);
-IPAddress gateway(192, 168, 0, 1);
+byte mac[] = { 0xDE, 0xAD, 0xBE, 0xEF, 0xFE, 0xEE };
+IPAddress ip(192, 168, 1, 103);
+IPAddress gateway(192, 168, 1, 1);
 IPAddress subnet(255, 255, 255, 0);
 
 EthernetClient ethClient;
 PubSubClient client(ethClient);
 
 // Replace with the MQTT broker IP and port
-const char* mqtt_server = "127.0.0.1";
+const char* mqtt_server = "192.168.1.214";
 const int mqtt_port = 1883;
 
+std::map<std::string, int> sensorPinsMap;
 
 // DHT sensor setup
-const long TEMPERATURE_READ_FREQUENCY = 60000;
-#define DHTTYPE DHT22
+const long TEMPERATURE_READ_FREQUENCY = 600000;
+#define DHTTYPE DHT11  //TODO
 DHT dhtD9(2, DHTTYPE);
 DHT dhtD2(3, DHTTYPE);
 
+//floor sensors setup
+const long FLOOR_TEMPERATURE_READ_FREQUENCY = 60000;
+#define D1_FLOOR_SENSOR_PIN A0
 
-// Pin numbers for floor temperature sensors
-const int floorTeperatureSensorPinD1 = 6;
+typedef enum {
+  SHUTTER_RELAY,
+  RELAY,
+  BME280_SENSOR,
+  NTC_TEMPERATURE_SENSOR_10K
+} objectType;
 
-
-// Pin numbers for flood sensors
-const int floodSensorPin = 21;
-
-
-const long SHUTTER_LONG_OPERATION_DURATION = 20000;
-const long SHUTTER_SHORT_OPERATION_DURATION = 15000;
-const long SHUTTER_ACTIVE_CHECK_FREQUENCY = 60000;
-ShutterRelay shutterM10(7, 8, SHUTTER_LONG_OPERATION_DURATION, &client, "M10");
-ShutterRelay shutterM11(9, 10, SHUTTER_LONG_OPERATION_DURATION, &client, "M11");
-ShutterRelay shutterM12(11, 12, SHUTTER_SHORT_OPERATION_DURATION, &client, "M12");
-ShutterRelay shutterM13(13, 14, SHUTTER_SHORT_OPERATION_DURATION, &client, "M13");
+std::map<std::string, objectType> objectsMap;
 
 
-/**
-  read all DHT sensors every TEMPERATURE_READ_FREQUENCY miliseconds
-*/
-void readDHTValues() {
-  static unsigned long lastReadMillis = 0;
-  if (currentMillis - lastReadMillis > TEMPERATURE_READ_FREQUENCY) {
-    readDHT(dhtD9, "D9");
-    readDHT(dhtD2, "D2");
+
+// ---------------------------------------------------------------------------
+
+void setup() {
+  Serial.begin(115200);
+  Serial.println("Starting device");
+  initializeShutterRelays();
+  initializeDhtSensors();
+  initializeFlorTemperatureSensors();
+
+  // Connect to Ethernet
+  Ethernet.begin(mac, ip, gateway, subnet);
+
+  // Connect to MQTT broker
+  client.setServer(mqtt_server, mqtt_port);
+  client.setCallback(callback);
+}
+
+void initializeShutterRelays() {
+  objectsMap["m10"] = SHUTTER_RELAY;
+  shutterRelaysHighMap["m10"] = M10_HIGH;
+  shutterRelaysLowMap["m10"] = M10_LOW;
+
+  objectsMap["M11"] = RELAY;  //TODO
+
+  shutterRelaysHighMap["M11"] = 23;
+  shutterRelaysLowMap["M11"] = 24;
+  shutterRelaysHighMap["M12"] = 25;
+  shutterRelaysLowMap["M12"] = 26;
+  shutterRelaysHighMap["M13"] = 27;
+  shutterRelaysLowMap["M13"] = 28;
+
+  // Iterate through the  map and set pinMode to OUTPUT for each pin
+  for (auto const& [deviceName, pinNumber] : shutterRelaysHighMap) {
+    pinMode(pinNumber, OUTPUT);
+  }
+  for (auto const& [deviceName, pinNumber] : shutterRelaysLowMap) {
+    pinMode(pinNumber, OUTPUT);
   }
 }
 
-/**
-  read DHT sensor humidity and temperature values and publish to topic
-*/
-void readDHT(DHT dht, String topic) {
-  float temperature = dht.readTemperature();
-  float humidity = dht.readHumidity();
-  if (isnan(temperature) || isnan(humidity)) {
-    Serial.println("Failed to read from DHT sensor");
-    return;
-  }
-  char topicTemperature[20];
-  snprintf(topicTemperature, 20, "/%d/temperature", topic);
-  char payloadTemperature[20];
-  snprintf(payloadTemperature, 20, "%.2f", temperature);
-  client.publish(topicTemperature, payloadTemperature);
-
-  char topicHumidity[20];
-  snprintf(topicHumidity, 20, "/%d/humidity", topic);
-  char payloadHumidity[20];
-  snprintf(payloadHumidity, 20, "%.2f", humidity);
-  client.publish(topicHumidity, payloadHumidity);
-
-  Serial.println("Published temperature and humidity for DHT sensor: " + topic);
+void initializeFlorTemperatureSensors() {
+  objectsMap["d1"] = NTC_TEMPERATURE_SENSOR_10K;
+  sensorPinsMap["d1"] = D1_FLOOR_SENSOR_PIN;
 }
 
-/**
-  every SHUTTER_ACTIVE_CHECK_FREQUENCY milliseconds check if any shutter is still marked as active but finished its operation 
-*/
-void checkShuttersStillActive() {
-  static unsigned long lastActiveMillis = 0;
-  if (currentMillis - lastActiveMillis > SHUTTER_ACTIVE_CHECK_FREQUENCY) {
-    shutterM10.checkActive();
-    shutterM11.checkActive();
-    shutterM12.checkActive();
-    shutterM13.checkActive();
-  }
-}
-
-/**
-  read all floor temperature sensors every TEMPERATURE_READ_FREQUENCY miliseconds
-*/
-void readFloorTemperatures() {
-  static unsigned long lastReadMillis = 0;
-  if (currentMillis - lastReadMillis > TEMPERATURE_READ_FREQUENCY) {
-    readFloorTemperatureValue(floorTeperatureSensorPinD1, "D1");
-  }
-}
-
-/**
-  read floor temperature value and publish to topic
-*/
-void readFloorTemperatureValue(int pin, String topic) {
-  char topicTemperature[20];
-  snprintf(topicTemperature, 20, "/%d/temperature", topic);
-  int temperature = analogRead(pin);
-  char payloadTemperature[20];
-  snprintf(payloadTemperature, 20, "%.2f", temperature);
-  client.publish(topicTemperature, payloadTemperature);
-}
-
-void readFloodSensorValues() {
-
- // Read the state of the flood sensor
-    int floodSensorState = digitalRead(floodSensorPin);
-
-    // Publish a message if a leak is detected
-    if (floodSensorState == LOW) {
-      client.publish("leakDetected", "1");
-    }
+void initializeDhtSensors() {
+  dhtD9.begin();
+  dhtD2.begin();
 }
 
 /**
   MQTT messages comming in handler 
 */
 void callback(char* topic, byte* payload, unsigned int length) {
+  Serial.println("Callback");
   String message;
   for (int i = 0; i < length; i++) {
     message += (char)payload[i];
@@ -237,59 +113,186 @@ void callback(char* topic, byte* payload, unsigned int length) {
   Serial.print(topic);
   Serial.print(". Message: ");
   Serial.println(message);
-
-  if(topic == "M10") {
-    shutterM10.control(message);
-  } else if(topic == "M11") {
-    shutterM11.control(message);
-  } else if(topic == "M12") {
-    shutterM12.control(message);
-  } else if(topic == "M13") {
-    shutterM13.control(message);
-  } else {
-    Serial.println("Nothing to do here.");
+  if (isShutterRelay(topic)) {
+    Serial.println("moveShutter");
+  } else if (isRelay(topic)) {
+    Serial.println("relay");
   }
 }
 
-void setup() {
-  Serial.begin(115200);
-
-  dhtD9.begin();
-  dhtD2.begin();
-
-  // Set the pin mode for the flood sensor
-  pinMode(floodSensorPin, INPUT);
-
-  pinMode(floorTeperatureSensorPinD1, INPUT);
-
-  Ethernet.begin(mac, ip, gateway, subnet);
-
-  // Connect to MQTT broker
-  client.setServer(mqtt_server, mqtt_port);
-  while (!client.connected()) {
-    if (client.connect("ArduinoClient")) {
-      Serial.println("Connected to MQTT broker");
-
-    } else {
-      Serial.println("Connection to MQTT broker failed, retrying...");
-      delay(5000);
-    }
-  }
-
-  shutterM10.connect();
-  shutterM11.connect();
-  shutterM12.connect();
-  shutterM13.connect();
+boolean isShutterRelay(char* deviceName) {
+  return objectsMap[deviceName] == SHUTTER_RELAY;
 }
+
+boolean isRelay(char* deviceName) {
+  return objectsMap[deviceName] == RELAY;
+}
+
+void moveShutter(char* shutterRelay, char* command) {
+}
+
+
+// ---------------------------------------------------------------------------
 
 void loop() {
   currentMillis = millis();
   // Keep the connection to the MQTT broker alive
+  if (!client.connected()) {
+    reconnectToMQTT();
+  }
   client.loop();
 
   readDHTValues();
-  readFloodSensorValues();
-  checkShuttersStillActive();
-  readFloorTemperatures();
+  readFloorSensors();
+}
+
+void reconnectToMQTT() {
+  while (!client.connected()) {
+    Serial.print("Attempting MQTT connection...");
+    if (client.connect("arduinoMega", "homeassistant", "", "arduinoMega/available", 0, true, "offline")) {
+      Serial.println("connected");
+      client.publish("arduinoMega/available", "online", true);
+      subscribeToTopics();
+    } else {
+      Serial.print("failed, rc=");
+      Serial.print(client.state());
+      Serial.println(" try again in 5 seconds");
+      delay(5000);
+    }
+  }
+}
+
+/**
+  read all DHT sensors every TEMPERATURE_READ_FREQUENCY miliseconds
+*/
+void readDHTValues() {
+  static unsigned long lastReadMillis = 0;
+  if (currentMillis - lastReadMillis > TEMPERATURE_READ_FREQUENCY) {
+    lastReadMillis = currentMillis;
+    readDHT(dhtD9, "d9");
+    readDHT(dhtD2, "d2");
+  }
+}
+
+/**
+  read all NTC 10k Ohm temperature sensors every FLOOR_TEMPERATURE_READ_FREQUENCY miliseconds
+*/
+void readFloorSensors() {
+  static unsigned long lastReadMillis = 0;
+  if (currentMillis - lastReadMillis > FLOOR_TEMPERATURE_READ_FREQUENCY) {
+    lastReadMillis = currentMillis;
+    for (auto const& [deviceName, objectType] : objectsMap) {
+      if (objectType != NTC_TEMPERATURE_SENSOR_10K) {
+        continue;
+      }
+      char* deviceChar = deviceName.c_str();
+      String device = deviceChar;
+      
+      int Vo;
+      float R1 = 10000;
+      float logR2, R2, T, Tc;
+      float c1 = 1.009249522e-03, c2 = 2.378405444e-04, c3 = 2.019202697e-07;
+      int samples = 5;
+      float value = 0;
+      for(int x = 0; x < samples; x++) {
+        Vo = analogRead(sensorPinsMap[deviceChar]);
+        R2 = R1 * (1023.0 / (float)Vo - 1.0);
+        logR2 = log(R2);
+        T = (1.0 / (c1 + c2*logR2 + c3*logR2*logR2*logR2));
+        Tc = T - 273.15;
+        value += Tc;
+        delay(500);      
+      }
+      value = value / samples;
+      
+      Serial.print(device);
+      Serial.print(" floor sensor Temperature: ");
+      Serial.print(value);
+      Serial.println(" C");
+
+      // char topicTemperature[20];
+      // String tempTopic = device;
+      // tempTopic.toCharArray(topicTemperature, 20);
+      // char payloadTemperature[20];
+      // String temperatureValue = String(value);
+      // temperatureValue.toCharArray(payloadTemperature, 20);
+      // client.publish(topicTemperature, payloadTemperature);
+      publishSensorValue(device, String(value));
+    }
+  }
+}
+
+/*
+  publish sensor value to device topic
+*/
+void publishSensorValue(String device, String value) {
+  int deviceLength = device.length() + 1;
+  char topic[deviceLength];
+  device.toCharArray(topic, deviceLength);
   
+  int valueLength = value.length() + 1;
+  char payload[valueLength];
+  value.toCharArray(payload, valueLength);
+  client.publish(topic, payload);
+}
+
+/**
+  read DHT sensor humidity and temperature values and publish to topic
+*/
+void readDHT(DHT dht, String topic) {
+  Serial.println(topic);
+  float temperature = dht.readTemperature();
+  float humidity = dht.readHumidity();
+  Serial.print("temperature: ");
+  Serial.print(temperature);
+  Serial.println(F("°C"));
+  Serial.print(", humidity: ");
+  Serial.println(humidity);
+  Serial.println(F("%"));
+  if (isnan(temperature) || isnan(humidity)) {
+    Serial.println("Failed to read from DHT sensor");
+    return;
+  }
+  char topicTemperature[20];//TODO publishSensorValue
+  String tempTopic = topic + "/temperature";
+  tempTopic.toCharArray(topicTemperature, 20);
+  char payloadTemperature[20];
+  String temperatureValue = String(temperature);
+  temperatureValue.toCharArray(payloadTemperature, 20);
+  client.publish(topicTemperature, payloadTemperature);
+
+  char topicHumidity[20];
+  String humTopic = topic + "/humidity";
+  humTopic.toCharArray(topicHumidity, 20);
+  char payloadHumidity[20];
+  String humidityValue = String(humidity);
+  humidityValue.toCharArray(payloadHumidity, 20);
+  client.publish(topicHumidity, payloadHumidity);
+
+  Serial.println("Published temperature and humidity for DHT sensor: " + topic);
+}
+
+void subscribeToTopics() {
+  subscribeShutterRelays();
+  subscribeRelays();
+}
+
+void subscribeShutterRelays() {
+  Serial.println("Subscribing shutter relays");
+  for (auto const& [deviceName, objectType] : objectsMap) {
+    if (objectType != SHUTTER_RELAY) {
+        continue;
+      }
+    client.subscribe(deviceName.c_str());
+  }
+}
+
+void subscribeRelays() {
+  Serial.println("Subscribing relays");
+  for (auto const& [deviceName, objectType] : objectsMap) {
+    if (objectType != RELAY) {
+      continue;
+    }
+    client.subscribe(deviceName.c_str());
+  }
 }
